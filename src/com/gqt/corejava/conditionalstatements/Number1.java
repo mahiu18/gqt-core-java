3a49c0d6bc9b008706585fdf759e8583a1e4c765
mahiu <mahiu@192.168.0.196>
2025-07-30 12:05:12 +0530

    The loaded Conditional statements Patterns

---
+++ src/com/gqt/corejava/conditionalstatements/Number1.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.gqt.corejava.conditionalstatements;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Number1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the count : ");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(j==(n/2) || i+j==(n/2)||i==(n-1)) 
+				{
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+        sc.close();
+	}
+
+}
---
+++ src/com/gqt/corejava/conditionalstatements/Number8.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.gqt.corejava.conditionalstatements;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class Number8 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the count : ");
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(i==0 || j==0 || j==(n-1) || i==(n-1) || i==(n/2)) 
+				{
+					System.out.print("# ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+        sc.close();
+	}
+}
